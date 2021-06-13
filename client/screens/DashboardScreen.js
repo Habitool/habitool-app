ed0390bb316700692486293eb8a7212ae3dfc98d
ubaig517 <ubaig517>
2021-06-13 12:31:24 -0400

    write habit tile and styling

--- client/screens/DashboardScreen.js
+++ client/screens/DashboardScreen.js
@@ -3,6 +3,7 @@ import '../stylesheets/screens/DashboardScreen.css';
 
 // Component Imports
 import StarterTile from '../components/StarterTile';
+import HabitTile from '../components/HabitTile';
 
 
 const DashboardScreen = () => {
@@ -10,6 +11,8 @@ const DashboardScreen = () => {
     <div className="dashboard__screen">
       <div className="dashboard__habit-tiles">
         <StarterTile />
+        <HabitTile />
+        <HabitTile />
       </div>
     </div>
   );
